a50107b4df68cefd7567613c3f20dc27177d6469
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2021-12-05 04:28:45 +0000

    MVP

--- task_one/task_one.js
+++ task_one/task_one.js
@@ -1,5 +1,3 @@
-let password = "jhkjhkj1h";
-
 // common passwords are read and stored in an array
 let commonPwds = [
   "password",
@@ -80,21 +78,25 @@ const getNumOfChanges = (password) => {
 
 // Test 1: Check if number of char is correct
 console.log(getNumOfChanges("Supercalifragilisticexpialidocious")); // Expect: 9
-console.log(getNumOfChanges("yP717")); // Expect: 2
+console.log(getNumOfChanges("yP717"),"\n"); // Expect: 2
 
 // Test 2: Check if lower, upper and digits are present
 console.log(getNumOfChanges("supercali")); // Expect: 2
 console.log(getNumOfChanges("SUPERCALI")); // Expect: 2
 console.log(getNumOfChanges("65765765889")); // Expect: 2
-console.log(getNumOfChanges("superc889")); // Expect: 1
+console.log(getNumOfChanges("superc889"),"\n"); // Expect: 1
 
 // Test 3: Check if char repeated 3 or more times
 console.log(getNumOfChanges("superccc")); // Expect: 2, Upper and digits missing but only 1 char to be changed to pass checkThriceRepeatedChar
 console.log(getNumOfChanges("ssssuper")); // Expect: 2
-console.log(getNumOfChanges("superrrrrrcalifff")); // Expect: 5
+console.log(getNumOfChanges("superrrrrrcalifff"),"\n"); // Expect: 5
 
 // Test 4: Check not common password
 console.log(getNumOfChanges("password")); // Expect: 8
 
 // Test 5: Check examples with multiple failures
-console.log(getNumOfChanges("1111111111111111111")); // Expect: 17
+console.log(getNumOfChanges("1111111111111111111"),"\n"); // Expect: 17
+
+// Test 6: Now test yourself!
+const password = "";
+console.log(getNumOfChanges(password));
---
+++ task_two/task_two/src/robot-controller.js
@@ -0,0 +1,182 @@
+import React, { useState } from 'react';
+
+function RobotController() {
+
+  const sideLength = 5;
+  const directions = [
+    "NORTH",
+    "EAST",
+    "SOUTH",
+    "WEST"
+  ];
+  const directionEmoji = [
+    "☝",  // NORTH
+    "👉",  // EAST
+    "👇",  // SOUTH
+    "👈",  // WEST
+  ];
+
+  // state hooks
+  const [positionDirection, setPositionDirection] = useState({ x: 0, y: 0, facing: -1 });
+  const [message, setMessage] = useState({ code: -1, text: "" });
+  const [command, setCommand] = useState("");
+
+  // possible commands
+
+  const place = (x, y, facing) => {
+    if (x >= 0 && x < sideLength && y >= 0 && y < sideLength) {
+      setPositionDirection({ x: x, y: y, facing: facing });
+      setMessage({ code: 1, text: `Robot moved to (${x},${y}) and is facing ${directions[facing]}` });
+    } else {
+      setMessage({ code: 0, text: 'Coordinate choosen not on grid' });
+    }
+  }
+
+  const move = () => {
+    let newPosition = {
+      x: positionDirection.x,
+      y: positionDirection.y,
+      facing: positionDirection.facing
+    }
+    switch (positionDirection.facing) {
+      case 0:
+        newPosition.y++;
+        break;
+      case 1:
+        newPosition.x++;
+        break;
+      case 2:
+        newPosition.y--;
+        break;
+      case 3:
+        newPosition.x--;
+        break;
+      default:
+    }
+    if (newPosition.x >= 0 && newPosition.x < sideLength &&
+      newPosition.y >= 0 && newPosition.y < sideLength) {
+      setPositionDirection(newPosition);
+      setMessage({ code: 1, text: `Robot moved to (${newPosition.x},${newPosition.y}) and facing ${directions[newPosition.facing]}` });
+    } else {
+      setMessage({ code: 0, text: 'Robot falls off the grid!' });
+    }
+  }
+
+  const left = () => {
+    setPositionDirection({
+      x: positionDirection.x,
+      y: positionDirection.y,
+      facing: (positionDirection.facing - 1 + 4) % 4
+    });
+  }
+
+  const right = () => {
+    setPositionDirection({
+      x: positionDirection.x,
+      y: positionDirection.y,
+      facing: (positionDirection.facing + 1) % 4
+    });
+  }
+
+  const report = () => {
+    setMessage({
+      code: 1,
+      text: `Robot is at (${positionDirection.x},${positionDirection.y}) and facing ${directions[positionDirection.facing]}`
+    });
+  }
+
+  const handleSubmit = () => {
+    if (command === "move()") {
+      move();
+    }
+    else if (command === "left()") {
+      left();
+    }
+    else if (command === "right()") {
+      right();
+    }
+    else if (command === "report()") {
+      report();
+    }
+    // input is assumed to be a recognised command
+    else {
+      // parsing the place() command
+      const parsedCommand = command.split(", ");
+      place(
+        parseInt(parsedCommand[0][parsedCommand[0].length - 1]),
+        parseInt(parsedCommand[1][parsedCommand[1].length - 1]),
+        directions.indexOf(parsedCommand[2].substring(1, parsedCommand[2].length - 2))
+      )
+    }
+  }
+
+  return (
+    <>
+      <div className="tabletop">
+        {(() => {
+          // length of grid side
+          let returnGrid = [];
+          for (let i = sideLength - 1; i >= 0; i--) {
+            returnGrid.push(<div className="grid-row">{(() => {
+              let returnRow = [];
+              for (let j = 0; j < sideLength; j++) {
+                if (positionDirection.x === j && positionDirection.y === i) {
+                  returnRow.push(
+                    <div className="grid-element" data-x={j} data-y={i}>
+                      <div className="direction-emoji">{directionEmoji[positionDirection.facing]}</div>
+                      <div className="element-label">({j},{i})</div>
+                    </div>
+                  )
+                } else {
+                  returnRow.push(
+                    <div className="grid-element" data-x={j} data-y={i}>
+                      <div className="element-label">({j},{i})</div>
+                    </div>)
+                }
+              }
+              return returnRow;
+            })()
+            }</div>);
+          }
+          return returnGrid;
+        })()}
+      </div>
+      <div className="message-input-container">
+        <div className={
+          message.code === 1
+            ? 'message message-success'
+            : message.code === 0
+              ? 'message message-failure'
+              : ''}>{message.text}</div>
+        <div>
+          <br />
+          <input
+            className="command-input"
+            placeholder="Type a command here"
+            type="text"
+            value={command}
+            onChange={(e) => setCommand(e.target.value)}
+          />
+          <input
+            className="command-input-submit"
+            type="submit"
+            onClick={(e) => handleSubmit()}
+          />
+        </div>
+      </div>
+    </>
+  )
+
+}
+
+export default RobotController;
+
+/*
+Limitions to input:
+
+  - Input is assumed to be a recognised command
+
+  - For the place command, there must be a space between arguments,
+  facing direction must be all caps and facing direction must have single quotes
+
+*/
